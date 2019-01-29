ab60c4ba1815869745e1b76d415925b525968f6b
huixisheng <469184346@qq.com>
2019-01-29 23:25:32 +0800

    feat: add x-do rem

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 ```
 npm link 或者使用绝对地址`YourWorkspaces/x-do-cli/bin/x-do-component`
 ```
+
+```
+sudo chmod +x bin/x-do-*
+DEBUG="*,-not_this" x-do rem src/image
+```
--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
+const path = require('path');
 const fse = require('fs-extra');
+const walkSync = require('walk-sync');
 const logger = require('../lib/log');
 
 function writeFile(filePath, fileContent) {
@@ -14,4 +16,44 @@ function writeFile(filePath, fileContent) {
   }
 }
 
-module.exports.writeFile = writeFile;
+function getFileList(args) {
+  if (!Array.isArray(args)) {
+    if (fs.existsSync(args)) {
+      args = [args];
+    } else {
+      logger.error('参数有误');
+    }
+  }
+  let fileList = [];
+
+  args.forEach((value) => {
+    let projectPath;
+    if (path.isAbsolute(value)) {
+      projectPath = value;
+    } else {
+      projectPath = path.join(process.cwd(), value);
+    }
+
+    let walkFiles = [];
+    if (fs.existsSync(projectPath)) {
+      if (fs.statSync(projectPath).isFile()) {
+        walkFiles.push(projectPath);
+      } else {
+        walkFiles = walkSync(projectPath, {
+          directories: true,
+          ignore: ['.DS_Store', '*.exe', '*.dmg', '*.app'],
+          globs: ['**/*.js', '**/*.css', '**/*.html', '**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.gif', '**/*.svg'],
+          // globs: ['**/*.(jpg|png|jpeg|gif|js|css|html)']
+        }).map(file => path.join(projectPath, file));
+      }
+      fileList = fileList.concat(walkFiles);
+    }
+  });
+
+  return fileList;
+}
+
+module.exports = {
+  writeFile,
+  getFileList,
+};
